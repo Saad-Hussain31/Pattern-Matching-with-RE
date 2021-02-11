84d7858e00d558eded2abba5c31fb9dbd7f4e0d7
Saad-Hussain31 <saadnasir31@gmail.com>
2021-02-11 15:35:26 +0500

    Add files via upload

---
+++ Pattern Matching with RE/phoneAndEmail.py
@@ -0,0 +1,47 @@
+
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\d{3}|\(\d{3}\))?     # area code
+    (\s|-|\.)?             # separator
+    (\d{3})                # first 3 digits
+    (\s|-|\.)              # separator
+    (\d{4})                # last 4 digits
+    (\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+    )''', re.VERBOSE)
+
+   #STEP 2: create a regex for emails
+
+emailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+ # username
+    @ # @ symbol
+    [a-zA-Z0-9.-]+ # domain name
+    (\.[a-zA-Z]{2,4}) # dot-something
+    )''', re.VERBOSE)
+
+    #STEP 3: Finding all matches in clipboard text
+
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+    phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+    if groups[8] != '':
+        phoneNum += ' x' + groups[8]
+    matches.append(phoneNum)
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+#     The phoneNum variable contains a string
+# built from groups 1, 3, 5, and 8 of the matched text v. (These groups are
+# the area code, first three digits, last four digits, and extension.)
+
+    #STEP 4: Join the Matches into a String for the Clipboard
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or emails found..')
+
